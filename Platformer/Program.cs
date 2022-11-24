4c4b660ad784126963531661562ba7217be792ee
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2022-11-24 14:06:05 +0100

    oakdfpjidjgosigj

--- Platformer/Program.cs
+++ Platformer/Program.cs
@@ -2,19 +2,20 @@
 
 Raylib.InitWindow(800, 700, "Platform Ghost");
 Raylib.SetTargetFPS(60);
-
-string currentScene = "start"; //Start, game, end
-
 float speed = 5.5f;
 
-Texture2D avatarImage = Raylib.LoadTexture("PlayerGhost.png");
+//Texture
+Texture2D avatarImage = Raylib.LoadTexture("images/PlayerGhost.png");
 Texture2D enemyImage = Raylib.LoadTexture("");
-Texture2D menu = Raylib.LoadTexture("Graveyard_menu.png");
+Texture2D menu = Raylib.LoadTexture("images/Graveyard_menu.png");
+Texture2D ammoBox = Raylib.LoadTexture("images/AmmoBox.png");
 
+//Hitboxes
 Rectangle playerRect = new Rectangle(0, 0, avatarImage.width, avatarImage.height);
 Rectangle trapRect = new Rectangle(700, 500, 64, 64);
-
 Rectangle enemyRect = new Rectangle(70, 70, enemyImage.width, enemyImage.height);
+
+string currentScene = "start"; //Start, game, end
 
 //Game
 while (Raylib.WindowShouldClose() == false)
@@ -38,5 +39,44 @@ while (Raylib.WindowShouldClose() == false)
             playerRect.y += speed;
         }
     }
+    else if (currentScene == "start")
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+        {
+            currentScene = "game";
+        }
+    }
+}
+
+//Grafik
+Raylib.BeginDrawing();
+Raylib.ClearBackground(Color.WHITE);
+
+if (currentScene == "game")
+{
+    // Raylib.DrawTexture(//backgorund,
+    //     0,
+    //     0,
+    //     Color.WHITE);
+
+    Raylib.DrawTexture(avatarImage,
+      (int)playerRect.x,
+      (int)playerRect.y,
+      Color.WHITE);
+    Raylib.DrawRectangleRec(trapRect, Color.RED);
+}
+else if (currentScene == "start")
+{
+    Raylib.DrawTexture(menu,
+    0,
+    0,
+    Color.WHITE);
+    Raylib.DrawText("Press [ENTER] to start", 75, 500, 30, Color.WHITE);
 
 }
+else if (currentScene == "end")
+{
+    Raylib.DrawText("Game over", 10, 10, 64, Color.RED);
+}
+
+Raylib.EndDrawing();
